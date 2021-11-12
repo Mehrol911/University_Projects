ac6880e12ebea012afc69c9de259e853284880f6
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-11-12 18:23:33 +0500

    Add files via upload

--- Sophomore year/Fall semester/Lab 8/HelloApplication.java
+++ Sophomore year/Fall semester/Lab 8/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.example.javafxdemo;
-
+// U2010046
+// MEKHROL BAZAROV
+// Section: 002
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -26,16 +28,16 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello!"); // this is a title name
 
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        Text scenetitle = new Text("Welcome U2010046");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20)); // here we give font size to the title
+        grid.add(scenetitle, 0, 0, 2, 1); // this is the scene of the title
 
         Label userName = new Label("User Name:");
         grid.add(userName, 0, 1);
@@ -48,14 +50,14 @@ public class HelloApplication extends Application {
 
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setPadding(new Insets(25, 25, 25, 25)); // here we set the padding in the center
 
-
-        Button btn = new Button("Submit");
+        Button btn = new Button("Submit"); // creating a button named Submit
+        Button clearBtn = new Button("Clear"); // creating a button named Clear
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
-
+        hbBtn.getChildren().add(clearBtn);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -65,9 +67,18 @@ public class HelloApplication extends Application {
             }
         });
 
+        clearBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                userTextField.clear();
+                pwBox.clear();
+
+            }
+        });
+
         grid.add(hbBtn, 1, 4);
 
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 300, 275); // 300 is the horizontal length of the scene, another is vertical
 
         stage.setScene(scene);
         stage.show();
